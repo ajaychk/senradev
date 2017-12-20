3afa555e791132bb564ab3e538726353fb56ccaf
ajaychk <ajay.kumar@chipmonk.in>
2017-12-20 11:03:23 +0530

    server setup to receive uplink

---
+++ app/rcv/rcvhttp.go
@@ -0,0 +1,32 @@
+package rcv
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func init() {
+	go initServer()
+}
+
+func initServer() {
+	http.HandleFunc("/uplink", handleUplink)
+	err := http.ListenAndServe(":9010", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleUplink(w http.ResponseWriter, r *http.Request) {
+	log.Println("uplink received")
+	var tmp interface{}
+
+	if err := json.NewDecoder(r.Body).Decode(&tmp); err != nil {
+		log.Println("invalid data received")
+		w.Write([]byte("invalid data received"))
+		return
+	}
+	log.Println("data received", tmp)
+	w.Write([]byte("ok"))
+}
